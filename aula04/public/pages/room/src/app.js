6ca6e9ec3336d6382d91a88060c24e9592c32783
Alexandre Akira <alexandre.akiraakao-ext@ab-inbev.com>
2021-06-19 03:08:08 -0300

    urls pointing to heroku app

--- aula04/public/pages/room/src/app.js
+++ aula04/public/pages/room/src/app.js
@@ -3,14 +3,17 @@ const onload = () => {
   const room = urlParams.get('room');
   console.log('this is the room', room)
 
-  const socketUrl = 'http://localhost:3000'
+  const socketUrl = 'https://vast-ravine-63985.herokuapp.com'
+  // const socketUrl = 'http://localhost:3000'
   const socketBuilder = new SocketBuilder({ socketUrl })
 
   const peerConfig = Object.values({
     id: undefined,
     config: {
-      port: 9000,
-      host: 'localhost',
+      host: 'polar-woodland-91487.herokuapp.com',
+      secure: true,
+      // port: 9000,
+      // host: 'localhost',
       path: '/'
     }
   })
